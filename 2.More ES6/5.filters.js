49fac1739d6c145eb0b683d825f8a7894071c93c
SoumenBaral <shumen.sb@gmail.com>
2023-06-29 01:01:53 +0600

    find filter done

---
+++ 2.More ES6/5.filters.js
@@ -0,0 +1,19 @@
+const numbers = [12, 5, 23, 45, 11, 18, 9, 55, 61, 1];
+const bigNumber = numbers.filter(number=>number>20);
+const tiny = numbers.filter(number=>number<10)
+// console.log(tiny);
+// console.log(bigNumber);
+
+const even = numbers.filter( num => num % 2 === 0);
+console.log(even);
+
+const products = [
+    {id: 1, name: 'laptop', price: 45000},
+    {id: 1, name: 'mobile', price: 80000},
+    {id: 1, name: 'watch', price: 35000},
+    {id: 1, name: 'tablet', price: 23000},
+];
+const expensive = products.filter(product=>product.price>40000);
+const expensive2 = products.filter(product => product.price < 100);
+// console.log(expensive);
+console.log(expensive2);
